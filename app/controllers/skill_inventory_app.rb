4bab7135616e6c18da8934cb9831f832d67f007a
Adam Caron <adamwcaron@gmail.com>
2015-07-07 13:28:14 -0600

    adds a dashboard

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,6 +1,10 @@
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
+  get '/' do
+    erb :dashboard
+  end
+
   get '/skills' do
     erb :index
   end
